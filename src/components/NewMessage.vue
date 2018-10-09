84341d8383d6fb4478d388badb78deefa060db01
Mike Peiman <mikepeiman@gmail.com>
2018-10-09 10:49:07 -0700

    Messages now being saved to Firestore collection with sender name and timestamp

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -3,22 +3,39 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New Message (hit enter to add)</label>
       <input type="text" name="new-message" v-model="newMessage">
+      <p class="feedback red-text" v-if="feedback">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'NewMessage',
   props: ['name'],
   data() {
     return {
-      newMessage: null
+      newMessage: null,
+      feedback: null
     }
   },
   methods: {
     addMessage() {
       console.log(this.newMessage, this.name, Date.now())
+      if(this.newMessage) {
+        db.collection('messages').add({
+          content: this.newMessage,
+          name: this.name,
+          timestamp: Date.now()
+        }).catch(err => {
+          console.log(err)
+        })
+        this.newMessage = null
+      } else {
+        this.feedback = "You must enter a message in order to send"
+      }
+      
     }
   }
 }
