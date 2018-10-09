b198b576e3031c2fdc36b11c9a32bb1ba7294e5b
Mike Peiman <mikepeiman@gmail.com>
2018-10-09 10:37:42 -0700

    Created NewMessage component for receiving new messages from user and sending to DB

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,21 +1,57 @@
 <template>
-  <div class="chat container">
-    <h2>{{ this.name }}'s Chat</h2>
+<div class="chat container">
+  <h2 class="center teal-text">{{ this.name }}'s Chat</h2>
+  <div class="card">
+    <div class="card-content">
+      <!-- ul.messages>li*3>span.teal-text{Name}+span.grey-text.text-darken-3{Message}+span.grey-text.time{Timestamp} -->
+      <ul class="messages">
+        <li>
+          <span class="teal-text">Name</span>
+          <span class="grey-text text-darken-3">Message</span>
+          <span class="grey-text time">Timestamp</span>
+        </li>
+      </ul>
+
+    </div>
+    <div class="card-action">
+      <NewMessage :name="name" /> <!-- note the colon for data binding -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Chat',
-    props: ['name'],
-    data() {
-      return {
+import NewMessage from '@/components/NewMessage'
 
-      }
+export default {
+  name: 'Chat',
+  props: ['name'],
+  components: { // register other included components
+    NewMessage
+  },
+  data() {
+    return {
+
     }
   }
+}
 </script>
 
 <style>
+.chat h2 {
+  font-size: 2.6em;
+}
 
+.chat span {
+  font-size: 1.4em;
+
+}
+
+.chat .time {
+  display: flex;
+}
+
+.message>li {
+  display: flex;
+}
 </style>
